7f66771ae9c81d1a632c80ea8b8474ca7a3b132d
Stas Kirichok <kirichok.stas@gmail.com>
2016-07-17 22:58:12 +0300

    Initial Commit

---
+++ HistoryMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HistoryMap
+//
+//  Created by Rumata on 7/17/16.
+//  Copyright © 2016 DarkMatter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
